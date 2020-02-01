a9510c9cea790d8a556a2aad8c14710f08bd9a7b
Justin Bard <justin@juloent.com>
2020-02-01 11:32:04 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='jblib',
-                version='1.6.5',
+                version='1.6.50',
                 description='JustBard\'s Python Utilities',
                 long_description=long_description,
                 long_description_content_type="text/markdown",
